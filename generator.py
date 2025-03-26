4f62ba7acff7cde1792765de6bd24bfc4d1f8c12
Johnsonkdonald <Johnson.k.donald@gmail.com>
2025-03-25 20:39:10 -0400

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,1 +1,11 @@
+from diffusers import StableDiffusionPipeline
+import torch
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+pipe.to(device)
+
+def generate_image(prompt):
+    image = pipe(prompt).images[0]
+    return image
